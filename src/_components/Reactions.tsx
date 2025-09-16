4473fe44c2b4d3ccb78174999360a574206ff736
Rui Gomes <ruisoft01@gmail.com>
2025-09-16 20:55:10 +0100

    subindo correções

--- src/_components/Reactions.tsx
+++ src/_components/Reactions.tsx
@@ -3,45 +3,42 @@ import { IoIosHeart } from "react-icons/io";
 import { AiOutlineLike } from "react-icons/ai";
 import { useEffect, useState } from "react";
 
-  export default function Reactions(){
-
-    const [love, setLove] = useState< number | null>(null);
-    const [like, setLike] = useState< number | null>(null);
+export default function Reactions() {
+  const [love, setLove] = useState<number>(0);
+  const [like, setLike] = useState<number>(0);
 
   useEffect(() => {
     const savedLove = localStorage.getItem("love");
     const savedLike = localStorage.getItem("like");
-    
-    setLove(savedLove? JSON.parse(savedLove): 0)
-    setLike(savedLike? JSON.parse(savedLike): 0)
-    
+
+    if (savedLove) setLove(JSON.parse(savedLove));
+    if (savedLike) setLike(JSON.parse(savedLike));
   }, []);
 
   useEffect(() => {
-    if (love != null) {
-      localStorage.setItem("dados", JSON.stringify(love));
-    }
+    localStorage.setItem("love", JSON.stringify(love));
   }, [love]);
 
-  useEffect(()=>{
-    if(like != null){
-        localStorage.setItem("dados", JSON.stringify(like))
-    }
-  },[like])
-    return(
-        <>
-        <div className="flex items-center gap-x-3 ">
-                  <button
-                    onClick={() => setLove(love + 1)}
-                    className="flex items-center gap-x-1"
-                  >
-                    <IoIosHeart className="text-red-500" />
-                    {love}Love
-                  </button>
-                  <button onClick={() => setLike(like + 1)} className="flex items-center gap-x-1">
-                    <AiOutlineLike />
-                    {like}Like
-                  </button>
-                </div></>
-    )
-  }
+  useEffect(() => {
+    localStorage.setItem("like", JSON.stringify(like));
+  }, [like]);
+
+  return (
+    <div className="flex items-center gap-x-3">
+      <button
+        onClick={() => setLove(love + 1)}
+        className="flex items-center gap-x-1"
+      >
+        <IoIosHeart className="text-red-500" />
+        {love} Love
+      </button>
+      <button
+        onClick={() => setLike(like + 1)}
+        className="flex items-center gap-x-1"
+      >
+        <AiOutlineLike />
+        {like} Like
+      </button>
+    </div>
+  );
+}
